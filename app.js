d9dd0e0fc8865cae9d362f0f5210c2e8c7dd3161
Krishna Patel <jinupatel1997@gmail.com>
2026-07-29 15:42:12 +0530

    Refactor app to use Express Router

--- app.js
+++ app.js
@@ -1,90 +1,30 @@
-import { readFile, writeFile } from "fs/promises";
+import express, { Router } from "express";
+import { shortenerRoutes } from "./routes/shortener.routes.js";
+
+//import router from "./routes/shortener.routes.js";
 //import { createServer } from "http";
-import path from "path";
-import crypto from "crypto";     //random value create
-import express from "express";
-import { url } from "inspector";
+
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const DATA_FILE = path.join("data", "links.json");
+
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-const loadLinks = async () => {
-    try {
-        const data = await readFile(DATA_FILE, "utf-8");
-        return JSON.parse(data);
-    } catch (error) {
-        if (error.code === "ENOENT") {    //Error NO ENTry
-            await writeFile(DATA_FILE, JSON.stringify({}));
-            return {};
-        }
-        throw error;
-    }
-}
-
-const saveLinks = async (links) => {
-    await writeFile(DATA_FILE, JSON.stringify(links));
-}
+//express router
+//app.use(Router);
+app.use(shortenerRoutes);
 
 
-app.get("/", async (req, res) => {
-    try {
-        const file = await readFile(path.join("views", "index.html"));
-        const links = await loadLinks();
-
-        const content = file.toString().replaceAll("{{shortened_urls}}",
-            Object.entries(links).map(([shortCode, url]) =>
-                `<li><a href="/${shortCode}" target="_blank">${req.host}/${shortCode}</a> - ${url}</li>`
-            )
-                .join("")
-        );
-        return res.send(content);
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Internal server error");
-    }
-})
-
-
-app.post("/", async (req, res) => {
-    try {
-        const { url, shortCode } = req.body;
-        const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
-        const links = await loadLinks();
-
-        if (links[finalShortCode]) {
-            return res.status(400).send("400 - Short code already exists. Please choose another.");
-        }
-
-        links[finalShortCode] = url;
-        await saveLinks(links);
-        return res.redirect("/?success=true");
-    } catch (error) {
-        console.error(err);
-        return res.status(500).send("Internal server error");
-    }
-})
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
 
 
 
-app.get("/:shortCode", async (req, res) => {
-    try {
-        const { shortCode } = req.params;
-        const links = await loadLinks();
 
-        if (!links[shortCode]) return res.status(404).send("404 error occurred");
-
-        return res.redirect(links[shortCode]);
-    } catch (error) {
-        console.error(err);
-        return res.status(500).send("Internal server error");
-    }
-})
 
 
 
@@ -182,9 +122,5 @@ app.get("/:shortCode", async (req, res) => {
 //         });
 //     }
 // });
-
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
 
 
