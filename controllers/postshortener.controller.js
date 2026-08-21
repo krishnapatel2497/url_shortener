6756143bd9b41749d9bb624d160a1c322f025a14
Krishna Patel <jinupatel1997@gmail.com>
2026-08-21 11:50:41 +0530

    Connect Users with Short Links

--- controllers/postshortener.controller.js
+++ controllers/postshortener.controller.js
@@ -1,34 +1,60 @@
-import crypto from "crypto"; //random value create.
+import crypto from "crypto";
+import { ObjectId } from "mongodb";
+
 import {
   getLinkByShortCode,
-  loadLinks,
+  getLinksByUserId,
   saveLinks,
 } from "../models/shortener.model.js";
 
+// Show only the logged-in user's short links
 export const getShortenerPage = async (req, res) => {
   try {
-    const links = await loadLinks();
+    // User must be logged in
+    if (!req.user) {
+      return res.redirect("/login");
+    }
 
-    return res.render("index", { links, host: req.host });
+    // Get only links created by this user
+    const links = await getLinksByUserId(req.user.id);
+
+    return res.render("index", {
+      links,
+      host: req.host,
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).send("Internal server error");
   }
 };
 
+// Create a new short link
 export const postURLShortener = async (req, res) => {
   try {
-    const { url, shortCode } = req.body;
-    const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
-    const links = await loadLinks();
+    // User must be logged in
+    if (!req.user) {
+      return res.redirect("/login");
+    }
 
-    if (links[finalShortCode]) {
+    const { url, shortCode } = req.body;
+
+    const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+
+    // Check whether short code already exists
+    const existingLink = await getLinkByShortCode(finalShortCode);
+
+    if (existingLink) {
       return res
         .status(400)
         .send("400 - Short code already exists. Please choose another.");
     }
 
-    await saveLinks({ url, shortCode: finalShortCode });
+    // Save the link with the logged-in user's ID
+    await saveLinks({
+      url,
+      shortCode: finalShortCode,
+      userId: new ObjectId(req.user.id),
+    });
 
     return res.redirect("/?success=true");
   } catch (error) {
@@ -37,13 +63,17 @@ export const postURLShortener = async (req, res) => {
   }
 };
 
+// Redirect short URL to original URL
 export const redirectToShortLink = async (req, res) => {
   try {
     const { shortCode } = req.params;
 
     const link = await getLinkByShortCode(shortCode);
 
-    if (!link) return res.status(404).send("404 - Short URL not found");
+    if (!link) {
+      return res.status(404).send("404 - Short URL not found");
+    }
+
     return res.redirect(link.url);
   } catch (error) {
     console.error(error);
--- models/shortener.model.js
+++ models/shortener.model.js
@@ -1,17 +1,36 @@
+import { ObjectId } from "mongodb";
 import { dbclient } from "../config/db-client.js";
 import { env } from "../config/env.js";
 
 const db = dbclient.db(env.MONGODB_DATABASE_NAME);
+
 const shortenerCollection = db.collection("shorteners");
 
+// Get all links
 export const loadLinks = async () => {
   return shortenerCollection.find().toArray();
 };
 
+
+// Save a new link
 export const saveLinks = async (link) => {
   return shortenerCollection.insertOne(link);
 };
 
+
+// Find link using shortCode
 export const getLinkByShortCode = async (shortCode) => {
-  return await shortenerCollection.findOne({ shortCode: shortCode });
+  return shortenerCollection.findOne({
+    shortCode: shortCode,
+  });
+};
+
+
+// Get only links created by a specific user
+export const getLinksByUserId = async (userId) => {
+  return shortenerCollection
+    .find({
+      userId: new ObjectId(userId),
+    })
+    .toArray();
 };
